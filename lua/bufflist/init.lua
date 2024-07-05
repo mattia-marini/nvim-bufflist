59bfafe7916169e78b3cef3a07c9ac1120de0b9e
Mattia Marini <marmattia26@gmail.com>
2024-07-05 02:32:18 +0200

    Added Bufflist command

--- lua/bufflist/init.lua
+++ lua/bufflist/init.lua
@@ -4,8 +4,8 @@ require("bufflist.buffers")
 require("bufflist.mappings")
 require("bufflist.actions")
 
-vim.api.nvim_set_keymap('n', '<Space>F', '', {callback = function () require("bufflist.actions").open_window() end})
 
+vim.api.nvim_create_user_command("Bufflist", function ( ) require("bufflist.actions").open_window() end, {})
 return {config = function(user_conf)
   local config_table = require("bufflist.config")
   for key, value in pairs(user_conf) do
